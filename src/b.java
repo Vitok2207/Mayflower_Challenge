00bf08bcf23b7cc1969a59af106210c3d6d054a4
Vitok2207 <47314375+Vitok2207@users.noreply.github.com>
2019-07-08 15:46:40 +0200

    Update b.java

--- src/b.java
+++ src/b.java
@@ -1,18 +1,22 @@
-public class b {
-    static {
+import java.util.Arrays;
 
-        int numbers[] = new int[]{1, 2, -3, 4, 5};
-        int smallest = numbers[0];
-        int biggest = numbers[0];
+public class Main {
 
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > biggest)
-                biggest = numbers[i];
-            else if (numbers[i] < smallest)
-                smallest = numbers[i];
+    public static void main(String[] args) {
+	
+        String s = "1 2 -3 4 5";
+        String[] s_array = s.split(" ");
+        int[] i_array = new int[s_array.length];
+        for(int i = 0; i < s_array.length; i++){
+            i_array[i] = Integer.parseInt(s_array[i]);
         }
+        Arrays.sort(i_array);
 
-        System.out.print(biggest);
-        System.out.print(" " + smallest);
+		for(int i = 0; i < i_array.length; i++){
+		//	System.out.println(i_array[i]);
+		}
+
+        System.out.print(i_array[i_array.length-1]);
+        System.out.print(" " + i_array[0]);
     }
 }
