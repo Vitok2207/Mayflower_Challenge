cc4ad384fdf972a4112cc425a0658a0a373c1acf
Vitok2207 <47314375+Vitok2207@users.noreply.github.com>
2019-07-08 15:50:14 +0200

    Update c.java

--- src/c.java
+++ src/c.java
@@ -1,4 +1,32 @@
-public class c {
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+        String s = "1 9 3 4 -5";
+        String[] s_array = s.split(" ");
+        int[] i_array = new int[s_array.length];
+        for(int i = 0; i < s_array.length; i++){
+            i_array[i] = Integer.parseInt(s_array[i]);
+        }
+        Arrays.sort(i_array);
+
+		for(int i = 0; i < i_array.length; i++){
+		//	System.out.println(i_array[i]);
+		}
+
+        System.out.print(i_array[i_array.length-1]);
+        System.out.print(" " + i_array[0]);
+    }
+}
+
+/*
+Eine weitere Methode um das Ergebnis zu bekommen.
+
+public class Main {
+    
+  public static void main(String[] args) {
 
     int numbers[] = new int[]{1, 9, 3, 4, -5};
     int smallest = numbers[0];
@@ -13,4 +41,6 @@ public class c {
 
         System.out.print(biggest);
         System.out.print(" " + smallest)
-}
+  }
+}    
+*/    
